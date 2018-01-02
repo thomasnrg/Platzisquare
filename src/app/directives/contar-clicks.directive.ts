819360479145e9372073cb6dd1d8371df23df714
thomasnrg <thomasnrgsen@gmail.com>
2018-01-02 01:08:14 -0400

    Host Listeners - escuchando los clicks de nuestos ancors

---
+++ src/app/directives/contar-clicks.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: 'a[contar-clicks]'
+})
+export class ContarClicksDirective {
+  clickN = 0;
+  @HostListener('click', ['$event.target']) onClick(btn){
+    console.log('a', btn, "Numero de Clicks:", ++this.clickN);
+  }
+}

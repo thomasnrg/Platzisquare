129beb7adfe9053cb4ce177a76ba20dad32370a4
thomasnrg <thomasnrgsen@gmail.com>
2018-01-02 00:44:49 -0400

    Directiva de atributo - Creando nuestra propia directiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Platzisquare';
   lugares:any = [
-    {cercania: 1, distancia: 1, active: true, nombre: 'Negocio de Carros'},
-    {cercania: 1, distancia: 1.8, active: true, nombre: 'Negocio de Mascotas'},
-    {cercania: 2, distancia: 5, active: true, nombre: 'Negocio de Telefonos'},
-    {cercania: 3, distancia: 10, active: false, nombre: 'Negocio de Casas'},
-    {cercania: 3, distancia: 35, active: true, nombre: 'Negocio de Muebles'},
-    {cercania: 3, distancia: 120, active: false, nombre: 'Negocio de Desarrollo web'}
+    {plan: 'pagado' ,cercania: 1, distancia: 1, active: true, nombre: 'Negocio de Carros'},
+    {plan: 'gratuito' ,cercania: 1, distancia: 1.8, active: true, nombre: 'Negocio de Mascotas'},
+    {plan: 'pagado' ,cercania: 2, distancia: 5, active: true, nombre: 'Negocio de Telefonos'},
+    {plan: 'gratuito' ,cercania: 3, distancia: 10, active: false, nombre: 'Negocio de Casas'},
+    {plan: 'pagado' ,cercania: 3, distancia: 35, active: true, nombre: 'Negocio de Muebles'},
+    {plan: 'pagado' ,cercania: 3, distancia: 120, active: false, nombre: 'Negocio de Desarrollo web'}
   ]
   lat:number = 4.8637461;
   lng:number = -75.0190383;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'
-
 import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective
   ],
   imports: [
     BrowserModule,

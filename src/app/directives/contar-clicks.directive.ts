e5c5c93f6d9620b27f00e2dbcc5f7434225d621b
thomasnrg <thomasnrgsen@gmail.com>
2018-01-02 01:18:29 -0400

    Host Binders - la opacidad no funciona :C

--- src/app/directives/contar-clicks.directive.ts
+++ src/app/directives/contar-clicks.directive.ts
@@ -1,11 +1,13 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: 'a[contar-clicks]'
+  selector: 'li[contar-clicks]'
 })
 export class ContarClicksDirective {
   clickN = 0;
+  @HostBinding('style.opacity') opacity:number = .1;
   @HostListener('click', ['$event.target']) onClick(btn){
     console.log('a', btn, "Numero de Clicks:", ++this.clickN);
+    this.opacity += .1;
   }
 }
